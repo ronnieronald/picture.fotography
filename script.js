f3558793b9abe208f1827d3a9691b399b6a2dacf
ronnieronald <ronnieronald748@gmail.com>
2025-03-19 21:52:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openModal(src) {
+    document.getElementById('modal-img').src = src;
+    document.getElementById('modal').style.display = 'flex';
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
